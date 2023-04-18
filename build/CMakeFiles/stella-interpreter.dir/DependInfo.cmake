18251dae7b7bffc40323cbd57f4ea468bb86182e
Wild-Queue <eshalagin@list.ru>
2023-04-18 13:09:18 +0300

    Week2

---
+++ build/CMakeFiles/stella-interpreter.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Eval.cpp" "CMakeFiles/stella-interpreter.dir/src/Eval.cpp.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Eval.cpp.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Interpret.cpp" "CMakeFiles/stella-interpreter.dir/src/Interpret.cpp.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Interpret.cpp.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Stella/Absyn.C" "CMakeFiles/stella-interpreter.dir/src/Stella/Absyn.C.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Stella/Absyn.C.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Stella/Buffer.C" "CMakeFiles/stella-interpreter.dir/src/Stella/Buffer.C.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Stella/Buffer.C.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Stella/Lexer.C" "CMakeFiles/stella-interpreter.dir/src/Stella/Lexer.C.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Stella/Lexer.C.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Stella/Parser.C" "CMakeFiles/stella-interpreter.dir/src/Stella/Parser.C.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Stella/Parser.C.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/Stella/Printer.C" "CMakeFiles/stella-interpreter.dir/src/Stella/Printer.C.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/Stella/Printer.C.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/TypeCheck.cpp" "CMakeFiles/stella-interpreter.dir/src/TypeCheck.cpp.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/TypeCheck.cpp.o.d"
+  "/home/egor/InnoSubjects/S23/ACCPA/Assignments/Latest/src/VisitTypeCheck.cpp" "CMakeFiles/stella-interpreter.dir/src/VisitTypeCheck.cpp.o" "gcc" "CMakeFiles/stella-interpreter.dir/src/VisitTypeCheck.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/stella-interpreter.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for stella-interpreter.
+# This may be replaced when dependencies are built.
